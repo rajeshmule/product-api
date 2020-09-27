c8da8cfd9e9298a956cbbad6449cc5b4de407ad2
Rajesh Mule <rajeshsmule@gmail.com>
2020-09-27 22:37:47 +0530

    Express middleware

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const dotEnv = require('dotenv');
+const cors = require('cors');
 
 dotEnv.config();
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
 app.get('/', (req,res, next)=>{
     res.send("hello")
-})
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {console.log(`Server listening on port ${port}`)});
+
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).send({status: 500, message: err.message, body: {}});
+});
