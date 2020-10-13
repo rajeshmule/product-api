dab78bf11a4e7690301bd31431756cb2b0163d17
Rajesh Mule <rajeshsmule@gmail.com>
2020-10-13 17:45:16 +0530

    compleated the product CURD

---
+++ api/helper/dbHelper.js
@@ -0,0 +1,20 @@
+const constants = require('../constants')
+const mongoose = require('mongoose')
+module.exports = {
+  formatMongoData: (data) => {
+    if (Array.isArray(data)) {
+      let newDataList = []
+      for (value of data) {
+        newDataList.push(value.toObject())
+      }
+      return newDataList
+    }
+    return data.toObject()
+  },
+
+  checkObjectId: (id) => {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new Error(constants.databaseMessage.INVALID_ID)
+    }
+  },
+}
